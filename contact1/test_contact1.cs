c14a9603a26a4cb86e206555932a5668ca57afbe
Andrew Harrington <anharrington@gmai.com>
2014-02-20 22:32:18 -0600

    List for contact2

--- contact1/test_contact1.cs
+++ contact1/test_contact1.cs
@@ -6,8 +6,10 @@ namespace IntroCS
    {
       public static void Main()
       {
-         Contact c1 = new Contact("Marie Ortiz", "773-508-7890", "mortiz2@luc.edu");
-         Contact c2 = new Contact("Otto Heinz", "773-508-9999", "oheinz@luc.edu");
+         Contact c1 = new Contact("Marie Ortiz", "773-508-7890", 
+                                  "mortiz2@luc.edu");
+         Contact c2 = new Contact("Otto Heinz", "773-508-9999", 
+                                  "oheinz@luc.edu");
          Console.WriteLine("Marie's full name: " + c1.GetName());
          Console.WriteLine("Her phone number: " + c1.GetPhone());
          Console.WriteLine("Her email: " + c1.GetEmail());         
--- contact2/test_contact2.cs
+++ contact2/test_contact2.cs
@@ -1,22 +1,30 @@
 using System;
+using System.Collections.Generic;
 
 namespace IntroCS
 {
    public class TestContact2
-   {
+   {                                        // main chunk                                           
       public static void Main()
       {
-         Contact c1 = new Contact("Marie Ortiz", "773-508-7890", "mortiz2@luc.edu");
-         Contact c2 = new Contact("Otto Heinz", "773-508-9999", "oheinz@luc.edu");
+         Contact c1 = new Contact("Marie Ortiz", "773-508-7890", 
+                                  "mortiz2@luc.edu");
+         Contact c2 = new Contact("Otto Heinz", "773-508-9999", 
+                                  "oheinz@luc.edu");
          Console.WriteLine("Marie's full name: " + c1.GetName());
          Console.WriteLine("Her phone number: " + c1.GetPhone());
          Console.WriteLine("Her email: " + c1.GetEmail());         
+         Console.WriteLine("All together:\n{0}", c1); 
          Console.WriteLine("Full contact info for Otto:");
          c2.Print(); 
-         c1.SetEmail ("maria.ortiz@gmail.com");
-         c2.SetPhone ("123-456-7890");
-         Console.WriteLine ("Now both with changes:\n" + c1); //no ToString!
-         Console.WriteLine ("No ToString needed with + or WriteLine:\n{0}", c2);
+         c1.SetEmail("maria.ortiz@gmail.com");
+         c2.SetPhone("123-456-7890");
+         Console.WriteLine("With changes and added contact:");
+         var allc = new List<Contact>(new Contact[] {c1, c2, 
+            new Contact("Amy Li", "847-111-2222", "amy.li22@yahoo.com")});
+         foreach(Contact c in allc) {
+            Console.WriteLine("\n"+c);
+         }
       }
-   }
+   }                                       // end of chunk
 }
