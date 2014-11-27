74a3f349b6e85967bce6bf0ed507750c3d36b27a
George K. Thiruvathukal <gkt@cs.luc.edu>
2014-11-27 09:49:36 -0600

    travis: One more try for 2.x and 3.x building using scripts in build matrix
    Signed-off-by: George K. Thiruvathukal <gkt@cs.luc.edu>

---
+++ .ci/install-mono-3.10.0.sh
@@ -0,0 +1,3 @@
+MONO_VER=3.10.0
+wget "http://download.mono-project.com/archive/${MONO_VER}/macos-10-x86/MonoFramework-MDK-${MONO_VER}.macos10.xamarin.x86.pkg"
+sudo installer -pkg "MonoFramework-MDK-${MONO_VER}.macos10.xamarin.x86.pkg" -target /
