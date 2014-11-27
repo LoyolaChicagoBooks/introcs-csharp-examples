152a301ec27518555499cffa2c8bcb2ebf14c991
George K. Thiruvathukal <gkt@cs.luc.edu>
2014-11-27 10:51:50 -0600

    travis: refactor build scripts and matrix for more generic Mono support on OS X
    Signed-off-by: George K. Thiruvathukal <gkt@cs.luc.edu>

--- .ci/install-mono-2.sh
+++ .ci/install-mono-2.sh
@@ -1,4 +1,4 @@
-MONO_VER=2.10.11
+MONO_VER=$1
 wget "http://download.mono-project.com/archive/${MONO_VER}/macos-10-x86/MonoFramework-MDK-${MONO_VER}.macos10.xamarin.x86.dmg"
 hdid "MonoFramework-MDK-${MONO_VER}.macos10.xamarin.x86.dmg"
 sudo installer -pkg "/Volumes/Mono Framework MDK ${MONO_VER}/MonoFramework-MDK-${MONO_VER}.macos10.xamarin.x86.pkg" -target /
