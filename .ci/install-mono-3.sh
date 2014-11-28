0d8ca460d4b847d52196aadae1dcfe5ac626381a
George K. Thiruvathukal <thiruvathukal@gmail.com>
2014-11-28 10:41:16 -0600

    build: Remove embedded binaries for nuget.exe and fetch for Travis build
    Signed-off-by: George K. Thiruvathukal <thiruvathukal@gmail.com>

--- .ci/install-mono-3.sh
+++ .ci/install-mono-3.sh
@@ -1,3 +1,9 @@
 MONO_VER=$1
+
+# Install Mono (OS X)
 wget "http://download.mono-project.com/archive/${MONO_VER}/macos-10-x86/MonoFramework-MDK-${MONO_VER}.macos10.xamarin.x86.pkg"
 sudo installer -pkg "MonoFramework-MDK-${MONO_VER}.macos10.xamarin.x86.pkg" -target /
+
+# Install NuGet (platform-independent)
+
+wget http://nuget.org/nuget.exe
