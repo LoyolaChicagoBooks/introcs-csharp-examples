0cce04f9e2292c0682b41c9b264dbb19141ea0a1
George K. Thiruvathukal <thiruvathukal@gmail.com>
2014-11-28 10:45:53 -0600

    travis: re-enable Mono 2.x building
    Signed-off-by: George K. Thiruvathukal <thiruvathukal@gmail.com>

--- .ci/install-mono-2.sh
+++ .ci/install-mono-2.sh
@@ -1,4 +1,11 @@
 MONO_VER=$1
+
+# Install Mono 2.x (OS X worker on Travis)
+
 wget "http://download.mono-project.com/archive/${MONO_VER}/macos-10-x86/MonoFramework-MDK-${MONO_VER}.macos10.xamarin.x86.dmg"
 hdid "MonoFramework-MDK-${MONO_VER}.macos10.xamarin.x86.dmg"
 sudo installer -pkg "/Volumes/Mono Framework MDK ${MONO_VER}/MonoFramework-MDK-${MONO_VER}.macos10.xamarin.x86.pkg" -target /
+
+# Install NuGet (platform-independent)
+
+wget http://nuget.org/nuget.exe
