bbdc26325ff93480b10576a7818d535d876bbb2d
Andrew Harrington <aharrin@luc.edu>
2014-12-18 01:28:08 -0600

    separated drivers to demo classes for searching, sorting

---
+++ searching/extract_from_string.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace IntroCS
+{
+   public class ExtractFromString
+   {
+      /// Return an array of non-empty strings that are separated
+      /// in the original string by any combination of commas and blanks.
+      /// Example:  ExtractItems("  extra  spaces,plus,  more, ") returns
+      /// an array containing {"extra", "spaces", "plus", "more"} 
+      public static string[] ExtractItems(string s)
+      {
+         s = s.Replace (",", " "); //only worry about blanks
+         int dataLength = 0;
+         string[] okEmptyStrings = s.Split(' '); 
+         foreach (string w in okEmptyStrings) { 
+            if (w.Length > 0) { //ending or consecutive blanks generate ""
+               dataLength++;
+            }
+         } 
+         string[] data = new string[dataLength];
+         int i = 0;  // next open index in data
+         foreach (string w in okEmptyStrings) { 
+            if (w.Length > 0) {  // ignore "" from Split
+               data [i] = w;
+               i++;
+            }
+         }
+         return data;
+
+      }
+      /// Return ints from space/comma separated integers in a string.
+      public static int[] IntsFromString(string input)
+      {
+         string[] integers = ExtractItems(input); 
+         int[] data = new int[integers.Length];
+         for(int i = 0; i < data.Length; i++) { 
+            data[i] = int.Parse(integers[i]);
+         }
+         return data;
+      }
+   }
+}
+
---
+++ searching/searching_demo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace IntroCS
+{
+   public class SearchingDemo
+   {
+      public static void Main()
+      {                                       // demo chunk     
+         string input = UI.PromptLine(
+           "Please enter integers, separated by spaces and/or comma: ");
+         int[] data = ExtractFromString.IntsFromString(input);
+         for (int i=0; i < data.Length; i++) {
+            Console.WriteLine("data[{0}]={1}", i, data[i]);
+         }
+         string prompt = 
+            "Please enter a number to find (blank line to end): ";
+         input = UI.PromptLine(prompt);
+         while (input.Length > 0) {
+            int searchItem = int.Parse(input);
+            int searchPos = UI.PromptIntInRange(
+               "At what position should the search start? ",
+               0, data.Length);
+            int foundPos = 
+               Searching.IntArrayLinearSearch(data,searchItem, searchPos);
+            if (foundPos < 0) {
+               Console.WriteLine("Item {0} not found", searchItem);
+            } 
+            else {
+               Console.WriteLine("Item {0} found at position {1}", 
+                  searchItem, foundPos);
+            }
+            input = UI.PromptLine(prompt);
+         }
+      }                                       // end demo chunk
+   }
+}
+
