be9eba32778d04482d311756841ad992b4c3ea98
Andrew Harrington <aharrin@luc.edu>
2014-12-21 21:07:16 -0600

    additions, tweaks, exercise moves through all the loop chapters

---
+++ reversed_print/reversed_print.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace IntroCS
+{
+   class ReversePrint
+   {
+      static void Main() //testing routine
+      {
+         string s = UIF.PromptLine("Enter a line: ");
+         Console.WriteLine ("Reversed:");
+         PrintReversed("\n"+s);
+      } 
+                                // chunk
+      /// Print s in reverse order; no extra newlines
+      static void PrintReversed(string s)
+      {
+         int i = s.Length-1;
+         while (i >= 0 ) {
+            Console.Write(s[i]);
+            i--;
+         }
+      }
+   }                           
+}
