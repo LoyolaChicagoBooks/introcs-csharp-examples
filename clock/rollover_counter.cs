51c008c967ff0f860d7916f4e6101f2ec2a727e7
Andrew Harrington <aharrin@luc.edu>
2015-01-16 03:08:10 -0600

    2 new interacting object classes in clock section

---
+++ clock/rollover_counter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace IntroCS
+{
+   /// class used twice in Clock
+   class RolloverCounter 
+   {
+      private int limit, count;
+
+      public RolloverCounter(int limit) 
+      {
+         this.limit = limit;
+         count = 0;  //for clarity - this is the default value
+      }
+                               
+      public int GetCount() 
+      {
+         return count;
+      }
+
+      public void SetCount(int count) 
+      {
+         this.count = count;
+      }
+                               
+      /// advance by one time tick 
+      /// eventually roll over at limit                        
+      public void Advance() 
+      {
+         count = (count + 1) % limit;
+      }
+   }                           
+}
+  
