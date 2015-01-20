5050ed1a0aa91b5bf63d7fbaea26a3db7d79d0bc
Andrew Harrington <aharrin@luc.edu>
2015-01-20 17:24:41 -0600

    averager

---
+++ averager/averager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace IntroCS
+{
+   /// a class that is more than a container
+   class Averager
+   {
+      private int nValues;
+      private double sum;
+
+      /// new Averager with no data
+      public Averager() 
+      {
+         Clear();
+      }
+
+      public void AddDatum(double value)
+      {
+         sum += value;
+         nValues++;
+      }
+
+      public int GetDataCount() 
+      {
+         return nValues;
+      }
+
+      /// Gets the average of the data
+      ///   or NaN if no data.
+      public double GetAverage() 
+      {
+         return sum/nValues;  // is NaN if nValues is 0
+      }
+
+      public void Clear()
+      {
+         sum = 0.0;
+         nValues = 0;
+      }
+
+      public override string ToString ()
+      {
+         return string.Format("items: {0}; average: {1}", GetDataCount(), GetAverage());
+      } 
+   }
+}
---
+++ averager/averager_demo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace IntroCS
+{
+   class AveragerDemo 
+   {
+      public static void Main() 
+      {                             // body chunk
+         Averager a = new Averager();
+         Console.WriteLine("New Averager: " + a);
+         foreach (double x in new[] {5.1, -7.3, 11.0, 3.7}) {
+            Console.WriteLine ("Next dataum " + x);
+            a.AddDatum (x);
+            Console.WriteLine("average {0} with {1} data values", 
+               a.GetAverage(), a.GetDataCount());
+         }
+         a.Clear();
+         Console.WriteLine("After clearing:");
+         Console.WriteLine("average {0} with {1} data values", 
+            a.GetAverage(), a.GetDataCount());
+      }                            // end chunk
+   }
+}
+
