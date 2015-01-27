b93749b2094547269d4676735c290fa9ce4aedee
Andrew Harrington <aharrin@luc.edu>
2015-01-27 02:24:03 -0600

    array exercise/example additions

---
+++ array_loop1/array_loop1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class ArrayLoop1
+{  //Play computer on this code and then test
+   public static void Main() 
+   {
+      int[] a = {1, 2, 3}, b = {7, 2, 3, 5},
+            c = {7, 0, 3, 2, 5};  
+      Console.WriteLine(foo(a, b, 2));
+      Console.WriteLine(foo(c, b, 4));
+   }
+
+   static int foo(int[] x, int[] y, int n) 
+   {
+      int k = 0;
+      for (int i = 0; i < n; i++) 
+         if (x[i] == y[i]) {
+            k++;
+         }
+      return k;
+   }
+}
---
+++ array_loop3/array_loop3.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class ArrayLoop3
+{  //Play computer on this code first and then test
+   public static void Main() 
+   {
+      string[] strArray = {"abcdefgh", "wxyz"};          
+      for (int i = 2; i < 4; i++) {
+         foreach(string s in strArray) {                               
+            Console.Write(s.Substring(s.Length/i) + "/");
+         }
+         Console.WriteLine();                                
+      }
+   }
+}
