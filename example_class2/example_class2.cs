bb9b0e7fa48d8a4f72037a2bb7dc2403c2613919
Andrew Harrington <aharrin@luc.edu>
2015-01-31 01:27:53 -0600

    modiofied codeproject interface syntax exsmples, new interface worked exercise

---
+++ example_class2/example_class2.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// elaboration allowing sorting via IComparable interface:
+class Example : IComparable<Example> //new: interface mention
+{
+   private int n; 
+   private double d; 
+
+   public Example(int n, double d) 
+   {
+      this.n = n; this.d = d; 
+   }
+
+   public int GetN() 
+   {
+      return n;
+   }
+
+   public double GetD() 
+   {
+      return d;
+   }
+  
+   public void SetN(int n) 
+   {
+      this.n = n;
+   }
+
+   public void SetD(double d) 
+   {
+      this.d = d;
+   }
+
+   public override string ToString() 
+   {
+      return "Example: n = " + n + ", d = " + d;
+   }
+
+   // new method required for the interface
+   public int CompareTo(Example other)
+   {
+      if (n != other.n) {
+         return n - other.n;
+      }
+      if (d > other.d) {
+         return 1;
+      }
+      if (d < other.d) {
+         return -1;
+      }
+      return 0;
+   }
+} // end of class Example
+
+
+class ExampleSortingDemo
+{
+   public static void Main()
+   {
+      var pairs = new List<Example> (new[] { new Example(5, 1.1), 
+         new Example(2, 9.9), new Example(5, -.3), 
+         new Example(22, 0), new Example(5, 1.1)});
+      foreach (Example e in pairs) {
+         Console.WriteLine (e);
+      }
+      Console.WriteLine("Sorted:");
+      pairs.Sort();
+      foreach(Example e in pairs) {
+         Console.WriteLine(e);
+      }
+   }
+} // end of class ExampleTest
---
+++ interface_syntax5/interface_demo5.cs
@@ -0,0 +1,45 @@
+﻿class InterfaceDemo5 : abc  
+{
+   public static void Main()
+   {
+      System.Console.WriteLine("Hello Interfaces");
+      InterfaceDemo5 refDemo = new InterfaceDemo5();
+      refDemo.xyz();
+      Sample refSample = new Sample();
+      refSample.xyz();    
+   }
+
+   public void xyz()
+   {
+      System.Console.WriteLine("In Demo :: xyz");
+   }  
+}
+
+interface abc
+{
+   void xyz();
+}
+
+class Sample : abc
+{
+   public void xyz()
+   {
+      System.Console.WriteLine("In Sample :: xyz");
+   }  
+}
+
+/*
+Now a second class, Sample, implementing interface abc is added,
+and we can create objects of both classes:
+
+refDemo is a reference to the object of class InterfaceDemo5. 
+refSample is a reference to the object of class Sample. 
+
+Both the classes implement the interface abc, with their own separate 
+implementation of the function xyz(). 
+Functions xyz() of the respective classes InterfaceDemo5 and 
+Sample are invoked through references refDemo and refSample.
+
+Code modified from Chetan Kudalkar's article
+http://www.codeproject.com/Articles/18743/Interfaces-in-C-For-Beginners
+*/
