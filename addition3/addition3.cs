1785007d6b343330d1f4ae76714eaf7eb8099b74
George K. Thiruvathukal <gkt@cs.luc.edu>
2015-07-14 14:49:22 -0500

    fix formatting for book
    Signed-off-by: George K. Thiruvathukal <gkt@cs.luc.edu>

--- addition3/addition3.cs
+++ addition3/addition3.cs
@@ -7,7 +7,8 @@ namespace IntroCS
       static string SumProblemString(int x, int y)
       {
          int sum = x + y;
-         string sentence = "The sum of " + x + " and " + y + " is " + sum + ".";
+         string sentence = "The sum of " + x + " and " + y +
+                           " is " + sum + ".";
          return sentence;
       }
 
@@ -15,8 +16,8 @@ namespace IntroCS
       {
          Console.WriteLine(SumProblemString(2, 3));
          Console.WriteLine(SumProblemString(12345, 53579));
-         int a = UIF.PromptInt("Enter an integer: ");      //NEW
-         int b = UIF.PromptInt("Enter another integer: "); //NEW
+         int a = UIF.PromptInt("Enter an integer: ");
+         int b = UIF.PromptInt("Enter another integer: ");
          Console.WriteLine(SumProblemString(a, b));
       }
    }
